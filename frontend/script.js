702f09a8c7c3ecfc03f3bad378ce7f0a028910b6
mudunuriharika <mudunuriharika3@gmail.com>
2026-05-10 16:42:47 +0530

    allow local files to connect to production backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.protocol === "file:"
+const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000/api/products" 
     : "https://project-12-d2tc.onrender.com/api/products";
 
